d925df82ebbcd83e5877ee63416b1522569dc782
Bryan <bryan_mcdermo@hotmail.com>
2017-10-31 23:25:44 +0000

    Added new file

---
+++ app/src/main/java/com/example/bryan/pushup/NewGitClass.java
@@ -0,0 +1,9 @@
+package com.example.bryan.pushup;
+
+/**
+ * Created by Bryan on 31/10/2017.
+ */
+
+public class NewGitClass {
+    private String notEmpty="Words";
+}
---
+++ app/src/main/java/com/example/bryan/pushup/Testing.java
@@ -0,0 +1,9 @@
+package com.example.bryan.pushup;
+
+/**
+ * Created by Bryan on 31/10/2017.
+ */
+
+public class Testing {
+    private int num=23;
+}
